e0e8b6696a254b52db8f1edee91d7b62613393ee
james <jamesedwarddevine@gmail.com>
2021-12-10 16:47:17 -0500

    added last task

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const data = require('./data.js');
 const sum = require('./sum.js');
+const cowsay = require('cowsay');
+const lodash = require('lodash');
 
 console.log(data.numbers);
 console.log(`${data.name} is from ${data.hometown}`);
 console.log(sum.sumNum(data.numbers));
+console.log(cowsay.say({text: "Hello World!"}));
+console.log(lodash.shuffle(data.numbers));
