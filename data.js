1fe109666ca9975c86a064dfe53d09653c5e9497
james <jamesedwarddevine@gmail.com>
2021-12-10 16:49:08 -0500

    changed export style to object notation for more than one element

--- data.js
+++ data.js
@@ -2,6 +2,9 @@ let numbers = [1, 1, 2, 3, 5, 8];
 let name = "Fibonacci"
 let hometown = "Pisa, Italy"
 
-module.exports.numbers = numbers;
-module.exports.name = name;
-module.exports.hometown = hometown;
+
+module.exports = {
+    numbers: numbers,
+    name: name,
+    hometown: hometown,
+}
